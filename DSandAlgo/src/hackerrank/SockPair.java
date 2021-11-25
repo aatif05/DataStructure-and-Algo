00a0664a4aecbacc040d9fa44ef177a170a1661c
aatif05 <mohammedaatifrizwan@gmail.com>
2021-11-25 12:58:49 +0530

    commit for conflict

--- DSandAlgo/src/hackerrank/SockPair.java
+++ DSandAlgo/src/hackerrank/SockPair.java
@@ -15,6 +15,10 @@ public class SockPair {
 	void sockMerchant() {
 		assertEquals(3, sockMerchant(1, Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20)));
 	}
+	@Test
+	void testing() {
+		assertEquals(2, sockMerchant(1, Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20)));
+	}
 
 	public static int sockMerchant(int n, List<Integer> ar) {
 		Map<Integer, Long> countMap = ar.stream().collect(Collectors.groupingBy(x -> x, Collectors.counting()));
